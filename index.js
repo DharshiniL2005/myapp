ce2f9fa5f8e59bc492ab5af8e60f453033470cef
DharshiniL2005 <dharshinil.cse2023@citchennai.net>
2026-03-31 13:43:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.write("Hello drlg 🚀 Your app is live!");
+  res.end();
+}).listen(3000);
+
+console.log("Server running on port 3000");
