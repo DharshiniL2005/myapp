c77f693bf383880ceacef0d8adbb2f3be42c3e66
DharshiniL2005 <162532244+DharshiniL2005@users.noreply.github.com>
2026-03-31 16:11:11 +0530

    Fix server listen syntax in index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const http = require("http");
 http.createServer((req, res) => {
   res.write("Hello drlg 🚀 Your app is live!");
   res.end();
-}).listen((3000, "0.0.0.0"));
+}).listen(3000, "0.0.0.0");
 
 console.log("Server running on port 3000");
